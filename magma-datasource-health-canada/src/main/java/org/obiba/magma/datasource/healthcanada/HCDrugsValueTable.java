ca08b2dc4fc9615b75819b45e47f34a49b213503
Yannick Marcon <yannick.marcon@obiba.org>
2013-06-07 16:37:48 -0400

    Value set implemented.

--- magma-datasource-health-canada/src/main/java/org/obiba/magma/datasource/healthcanada/HCDrugsValueTable.java
+++ magma-datasource-health-canada/src/main/java/org/obiba/magma/datasource/healthcanada/HCDrugsValueTable.java
@@ -7,21 +7,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+import org.obiba.magma.MagmaDate;
 import org.obiba.magma.MagmaRuntimeException;
 import org.obiba.magma.NoSuchValueSetException;
 import org.obiba.magma.Timestamps;
 import org.obiba.magma.Value;
 import org.obiba.magma.ValueSet;
 import org.obiba.magma.ValueTable;
+import org.obiba.magma.ValueType;
 import org.obiba.magma.Variable;
 import org.obiba.magma.VariableEntity;
 import org.obiba.magma.support.AbstractValueTable;
+import org.obiba.magma.support.VariableEntityBean;
 import org.obiba.magma.type.DateTimeType;
+import org.obiba.magma.type.DateType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,6 +43,8 @@ public class HCDrugsValueTable extends AbstractValueTable {
 
   private static final Logger log = LoggerFactory.getLogger(HCDrugsValueTable.class);
 
+  private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+
   static final String DRUG_ENTITY_TYPE = "Drug";
 
   static final String ALL_FILES_ZIP_URL
@@ -43,6 +53,9 @@ public class HCDrugsValueTable extends AbstractValueTable {
   private static final Charset WESTERN_EUROPE = Charset.availableCharsets().get("ISO-8859-1");
 
   private File zsource;
+
+  // source file name / entity id / value set
+  private Map<String, Map<String, String[]>> valueSets = Maps.newHashMap();
 
   public HCDrugsValueTable(HCDatasource datasource) {
     super(datasource, "Drugs");
@@ -141,8 +154,6 @@ public class HCDrugsValueTable extends AbstractValueTable {
 
     private final VariableEntity entity;
 
-    private Map<String,String[]> values = Maps.newHashMap();
-
     private HCDrugsValueSet(VariableEntity entity) {
       this.entity = entity;
     }
@@ -151,14 +162,111 @@ public class HCDrugsValueTable extends AbstractValueTable {
       String sourceFile = variable.getAttributeStringValue("file");
       int column = Integer.parseInt(variable.getAttributeStringValue("column"));
 
-      String[] vals = values.get(sourceFile);
-      if (vals == null) {
-        // TODO fetch data from file
-        //values.put(sourceFile,);
+      String[] valueSet = getValueSet(sourceFile);
+      if(valueSet == null) {
+        return variable.isRepeatable() ? variable.getValueType().nullSequence() : variable.getValueType().nullValue();
       }
 
-      String strVal = vals[column];
-      return variable.isRepeatable() ? variable.getValueType().sequenceOf(strVal) : variable.getValueType().valueOf(strVal);
+      return variable.isRepeatable()
+          ? getValueSequence(variable.getValueType(), valueSet[column])
+          : getValue(variable.getValueType(), valueSet[column]);
+    }
+
+    /**
+     * Load the data in memory if not already done.
+     *
+     * @param sourceFile
+     * @return
+     */
+    private String[] getValueSet(String sourceFile) {
+      Map<String, String[]> sourceValueSets = valueSets.get(sourceFile);
+      try {
+        if(sourceValueSets == null) {
+          sourceValueSets = Maps.newHashMap();
+          valueSets.put(sourceFile, sourceValueSets);
+          CSVReader reader = getEntryReader(sourceFile);
+          String[] nextLine;
+          while((nextLine = reader.readNext()) != null) {
+            nextLine = normalize(nextLine);
+            VariableEntity nextEntity = new VariableEntityBean(DRUG_ENTITY_TYPE, nextLine[0]);
+            if(getVariableEntityProvider().getVariableEntities().contains(nextEntity)) {
+              if(sourceValueSets.containsKey(nextLine[0])) {
+                // merge lines
+                sourceValueSets.put(nextLine[0], merge(sourceValueSets.get(nextLine[0]), nextLine));
+              } else {
+                sourceValueSets.put(nextLine[0], nextLine);
+              }
+            }
+          }
+          reader.close();
+        }
+      } catch(IOException e) {
+        throw new MagmaRuntimeException("Unable to read source file: " + sourceFile, e);
+      }
+
+      return sourceValueSets.get(entity.getIdentifier());
+    }
+
+    /**
+     * Handle date format and deparse lines that were abusively merged.
+     *
+     * @param type
+     * @param value
+     * @return
+     */
+    private Value getValue(ValueType type, String value) {
+      if(value == null || value.isEmpty()) return type.nullValue();
+
+      if(type.equals(DateType.get())) {
+        try {
+          return DateType.get().valueOf(dateFormat.parse(value));
+        } catch(ParseException e) {
+          e.printStackTrace();
+          return DateType.get().nullValue();
+        }
+
+      } else {
+        // case of abusive merging of lines
+        if(value.indexOf('|') != -1) {
+          String[] values = value.split("\\|");
+          if(values.length > 0) {
+            return getValue(type, values[0]);
+          } else {
+            return type.nullValue();
+          }
+        }
+        return type.valueOf(value);
+      }
+
+    }
+
+    /**
+     * Deparse multiple values resulting from the merge.
+     *
+     * @param type
+     * @param value
+     * @return
+     */
+    private Value getValueSequence(ValueType type, String value) {
+      List<Value> values = new ArrayList<Value>();
+      for(String val : value.split("\\|")) {
+        values.add(getValue(type, val));
+      }
+      return type.sequenceOf(values);
+    }
+
+    /**
+     * When there are multiple values, it is on multiple lines (therefore some data are abusively repeated).
+     *
+     * @param previousLine
+     * @param nextLine
+     * @return
+     */
+    private String[] merge(String[] previousLine, String[] nextLine) {
+      for(int i = 0; i < nextLine.length; i++) {
+        previousLine[i] = previousLine[i] + "|" + nextLine[i];
+      }
+      return previousLine;
     }
 
     @Override
@@ -173,7 +281,30 @@ public class HCDrugsValueTable extends AbstractValueTable {
 
     @Override
     public Timestamps getTimestamps() {
-      return null;
+      return new Timestamps() {
+        @Nonnull
+        @Override
+        public Value getLastUpdate() {
+          Value updated = getValue(getVariable("LAST_UPDATE_DATE"));
+          if (updated.isNull()) return HCDrugsValueTable.this.getTimestamps().getLastUpdate();
+          return updated;
+        }
+
+        @Nonnull
+        @Override
+        public Value getCreated() {
+          Value created = getLastUpdate();
+          Value history = getValue(getVariable("HISTORY_DATE"));
+          if (history.isNull()) return created;
+
+          for (Value val : history.asSequence().getValues()) {
+            if (((MagmaDate)val.getValue()).asDate().before(((MagmaDate)created.getValue()).asDate())) {
+              created = val;
+            }
+          }
+          return created;
+        }
+      };
     }
   }
 }
--- magma-datasource-health-canada/src/test/java/org/obiba/magma/datasource/healthcanada/HCDatasourceTest.java
+++ magma-datasource-health-canada/src/test/java/org/obiba/magma/datasource/healthcanada/HCDatasourceTest.java
@@ -7,6 +7,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.obiba.magma.MagmaEngine;
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueSet;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.Variable;
 import org.obiba.magma.VariableEntity;
@@ -39,6 +41,24 @@ public class HCDatasourceTest {
     Assert.assertTrue(entities.size()>13000);
     List<Variable> variables = Lists.newArrayList(drugs.getVariables());
     Assert.assertEquals(53, variables.size());
+  }
+
+  @Test
+  public void readDatasource() {
+    HCDatasource ds = new HCDatasource("bar");
+    Initialisables.initialise(ds);
+    ValueTable drugs = ds.getValueTable("Drugs");
+    Set<VariableEntity> entities = drugs.getVariableEntities();
+    ValueSet valueSet = drugs.getValueSet(entities.iterator().next());
+    for (Variable variable : drugs.getVariables()) {
+      System.out.println(variable.getName() + "=" + drugs.getValue(variable,valueSet));
+    }
+    Variable currentStatusFlag = drugs.getVariable("CURRENT_STATUS_FLAG");
+    Value currentStatusFlagValue = drugs.getValue(currentStatusFlag, valueSet);
+    Assert.assertTrue(currentStatusFlagValue.isSequence());
+    Assert.assertEquals("\"N\",\"Y\",\"N\",\"N\",\"N\",\"N\",\"N\",\"N\"",currentStatusFlagValue.toString());
+    Assert.assertEquals("2012-06-27", valueSet.getTimestamps().getLastUpdate().toString());
+    Assert.assertEquals("1996-09-23", valueSet.getTimestamps().getCreated().toString());
   }
 
 }
